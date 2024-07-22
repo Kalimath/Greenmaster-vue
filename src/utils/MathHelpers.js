468d1d567533327eb4b0e8d89749e34bafdde6c0
mathieu <mathieu_broe@yahoo.com>
2024-07-22 22:28:00 +0200

    round function improvements and cleanup

--- src/utils/MathHelpers.js
+++ src/utils/MathHelpers.js
@@ -1,11 +1,11 @@
 export function round(unroundedValue, precision = 5) {
     let roundedValue = unroundedValue;
-    console.log(`Rounding x: ${this.x} and y: ${this.y} with precision of ${precision}`)
+    if (typeof unroundedValue !== "number") throw new TypeError("Input value must be a number")
+    if (precision <= 0) throw new RangeError("Precision must be a positive integer")
     if (precision <= 0) throw new RangeError("Cannot round to a precision less than or equal to 0")
 
     const rest = unroundedValue % precision;
     const restPercentage = (rest / precision) * 100;
-    console.log(`Rest: ${rest}, restPercentage: ${restPercentage}`)
     restPercentage >= 50
         ? roundedValue += precision - rest
         : roundedValue -= rest
